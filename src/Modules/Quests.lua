fdcd854dc10a1c3e5863788850835c3370ed5887
ice cube tray <icecubetray@users.noreply.github.com>
2020-01-08 13:30:09 -0500

    Consolidate quest system manipulation commands

--- src/Modules/Quests.lua
+++ src/Modules/Quests.lua
@@ -1,94 +1,70 @@
-jester.deactivateQuest = function (cmdline)
-	local args = kcdfw.parseCmdline(cmdline);
-	local quest = (args["q"] or args["quest"]);
-	if not quest then
-		kcdfw.logError(jester.package.name, "Invalid quest given");
-		return;
-	end
+-- TODO: move following section to KCDFW upon proper testing and expansion
+-- <move_to_kcdfw>
 
+jester.disableQuest = function(quest)
 	QuestSystem.DeactivateQuest(quest);
 end
 
-kcdfw.registerCommand(
-	"jester_quest_deactivate",
-	"jester.deactivateQuest(%line)",
-	"Deactivate the given quest.",
-	"-q <quest>"
-);
-
-
-jester.activateQuest = function (cmdline)
-	local args = kcdfw.parseCmdline(cmdline);
-	local quest = (args["q"] or args["quest"]);
-	if not quest then
-		kcdfw.logError(jester.package.name, "Invalid quest given");
-		return;
-	end
-
+jester.enableQuest = function(quest)
 	QuestSystem.ActivateQuest(quest);
 end
 
-kcdfw.registerCommand(
-	"jester_quest_activate",
-	"jester.activateQuest(%line)",
-	"Activate the given quest.",
-	"-q <quest>"
-);
-
-
-jester.setQuestObjective = function (cmdline)
-	local args = kcdfw.parseCmdline(cmdline);
-
-	local quest = (args["q"] or args["quest"]);
-	if not quest then
-		kcdfw.logError(jester, "Invalid quest given");
-		return;
-	end
-
-	local obj = (args["o"] or args["obj"] or args["objective"]);
-	if not obj then
-		kcdfw.logError(jester, "Invalid objective given");
-		return;
-	end
-
-	QuestSystem.StartObjective(quest, obj);
-end
-
-kcdfw.registerCommand(
-	"jester_quest_set_objective",
-	"jester.resetQuest(%line)",
-	"Starts the specified objective of the specified quest.",
-	"-q <quest> [-d]"
-);
-
-
-jester.resetQuest = function (cmdline)
-	local args = kcdfw.parseCmdline(cmdline);
-
-	local quest = (args["q"] or args["quest"]);
-	if not quest then
-		kcdfw.logError(jester, "Invalid quest given");
-		return;
-	end
-
-	local deact = (args["d"] or args["deactivate"]);
-
-	if deact then
-		jester.deactivateQuest(cmdline);
-	end
-
+jester.resetQuest = function(quest)
 	QuestSystem.ResetQuest(quest);
-	QuestSystem.StartQuest(quest);
-	QuestSystem.StartObjective(quest, 1);
+end
 
-	if deact then
-		jester.activateQuest(cmdline);
+jester.startQuest = function(quest)
+	QuestSystem.StartQuest(quest);
+end
+
+jester.startQuestObjective = function(quest, objective)
+	QuestSystem.StartObjective(quest, objective);
+end
+
+-- </move_to_kcdfw>
+
+
+jester.cmdQuest = function(args)
+	local enable = (args.enable or args.e);
+	local disable = (args.disable or args.d);
+	local reset = (args.reset or args.r);
+	local start = (args.start or args.s);
+	local objective = (args.objective or args.obj or args.o);
+	local quest = (args.quest or args.q);
+
+	-- TODO: sanity checking
+
+	if disable then
+		jester.disableQuest(quest);
+	end
+
+	if reset then
+		jester.resetQuest(quest);
+	end
+
+	if enable then
+		jester.enableQuest(quest);
+	end
+
+	if start then
+		jester.startQuest(quest);
+	end
+
+	if objective then
+		jester.startQuestObjective(quest, ((kcdfw.isInt(objective) and objective) or 1));
 	end
 end
 
 kcdfw.registerCommand(
-	"jester_quest_reset",
-	"jester.resetQuest(%line)",
-	"Reset the given quest. Use -d to also deactivate and reactivate the quest.",
-	"-q <quest> [-d]"
+	"jester_quest",
+	"jester.cmdQuest(cmdtab(%line))",
+	"Quest system manipulation.",
+	{
+		e = "Enable the specified quest.",
+		r = "Reset the specified quest.",
+		d = "Disable the specified quest.",
+		s = "Start the specified quest.",
+		o = "Start the given objective on the specified quest.",
+		q = "Specify quest to act on."
+	}
 );
