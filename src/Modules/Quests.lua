25d2310e3fe7f71286ac6edcb7919aee2f14b3fa
ice cube tray <icecubetray@users.noreply.github.com>
2020-01-08 14:59:10 -0500

    Improve jester_quest implementation

--- src/Modules/Quests.lua
+++ src/Modules/Quests.lua
@@ -3,25 +3,32 @@
 
 jester.disableQuest = function(quest)
 	QuestSystem.DeactivateQuest(quest);
+	kcdfw.logVerbose(jester, "Quest disabled: %s", quest);
 end
 
 jester.enableQuest = function(quest)
 	QuestSystem.ActivateQuest(quest);
+	kcdfw.logVerbose(jester, "Quest enabled: %s", quest);
 end
 
 jester.resetQuest = function(quest)
 	QuestSystem.ResetQuest(quest);
+	kcdfw.logVerbose(jester, "Quest reset: %s", quest);
 end
 
 jester.startQuest = function(quest)
 	QuestSystem.StartQuest(quest);
+	kcdfw.logVerbose(jester, "Quest started: %s", quest);
 end
 
 jester.startQuestObjective = function(quest, objective)
 	QuestSystem.StartObjective(quest, objective);
+	kcdfw.logVerbose(jester, "Quest objective started: %s, %u", quest, objective);
 end
 
 -- </move_to_kcdfw>
+
+
 
 
 jester.cmdQuest = function(args)
@@ -32,28 +39,54 @@ jester.cmdQuest = function(args)
 	local objective = (args.objective or args.obj or args.o);
 	local quest = (args.quest or args.q);
 
-	-- TODO: sanity checking
+
+	if reset then
+		if not objective then
+			objective = 1
+		end
+	end
+
+	if objective and not kcdfw.isInt(objective) then
+		kcdfw.logError(jester, "Objective not a valid integer: %s", tostring(objective));
+		return;
+	end
+
+
+	local didSomething = false;
 
 	if disable then
 		jester.disableQuest(quest);
+		didSomething = true;
 	end
 
 	if reset then
 		jester.resetQuest(quest);
+		didSomething = true;
 	end
 
 	if enable then
 		jester.enableQuest(quest);
+		didSomething = true;
 	end
 
 	if start then
 		jester.startQuest(quest);
+		didSomething = true;
 	end
 
 	if objective then
-		jester.startQuestObjective(quest, ((kcdfw.isInt(objective) and objective) or 1));
+		jester.startQuestObjective(quest, objective);
+		didSomething = true;
+	end
+
+
+	if didSomething then
+		kcdfw.logInfo(jester, "Done.");
+	else
+		kcdfw.logWarning(jester, "No operation was executed.");
 	end
 end
+
 
 kcdfw.registerCommand(
 	"jester_quest",
